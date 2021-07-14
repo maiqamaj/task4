5371f5edd735549d77833eefca7fcd7dba29c70a
maiqamaj <maiqamaj952@gmail.com>
2021-07-15 00:40:25 +0300

    Task4

---
+++ src/jo/secondstep/solid/openclosed/correct/Java.java
@@ -0,0 +1,16 @@
+package jo.secondstep.solid.openclosed.correct;
+
+public class Java implements  LanguageProgramming {
+
+	@Override
+	public void getDescriptionLanguageProgramming() {
+		System.out.println("Java is an object-oriented programming language "
+				+ "that produces software for multiple platforms." + " When a programmer writes a Java application,"
+				+ " the compiled code (known as bytecode) " + "runs on most operating systems (OS),"
+				+ " including Windows," + " Linux and Mac OS.");
+
+		
+	}
+
+	
+}
