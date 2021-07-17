d9c50fd7dc86e5bf1ac0f79442207842f0b9df70
maiqamaj <maiqamaj952@gmail.com>
2021-07-17 22:00:08 +0300

    Task4

--- src/secondstep/sloid/liskovpubstitution/correct/passwordscan.java
+++ src/secondstep/sloid/liskovpubstitution/correct/passwordscan.java
@@ -1,29 +1,23 @@
 package secondstep.sloid.liskovpubstitution.correct;
 
-public class passwordscan implements  safe{
-	
+public class passwordscan implements safe {
+
 	private int Password;
 
 	@Override
 	public void openSafe(passwordProvider PasswordProvider, Object value) {
-		pass P=new pass(value);
-		
-          Password= P.getpass();
-          System.out.println("--------password scanning--------");
-          System.out.println("Safe is opened");
-          
-	
-        	  
-		
+		pass P = new pass(value);
+
+		Password = P.getpass();
+		System.out.println("--------password scanning--------");
+		System.out.println("Safe is opened");
+
 	}
 
 	@Override
 	public void closeSafe() {
 		System.out.println("Safe is closed");
-		
-	}
 
-	
-	
+	}
 
 }
